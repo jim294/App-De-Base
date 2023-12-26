b75f213d77e77fb213a9b8806db47b430021d431
Jean-Marie <jeanmariedeschamps25@gmail.com>
2023-12-26 12:20:00 +0100

    app de base

---
+++ src/features/posts/AddPostForm.js
@@ -0,0 +1,10 @@
+import { useState } from "react";
+
+const AddPostForm = () => {
+    const [title, setTitle] = useState('')
+  return (
+    <div>AddPostForm</div>
+  )
+}
+
+export default AddPostForm
